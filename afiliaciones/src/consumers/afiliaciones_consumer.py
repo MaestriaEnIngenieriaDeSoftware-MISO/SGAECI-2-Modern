54a191d796576b47d541448b3d28a767533d88cb
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2025-07-27 18:29:53 -0500

    refactor: move ActualizarAfiliacion to commands and add Redis consumer for updates

---
+++ afiliaciones/src/consumers/afiliaciones_consumer.py
@@ -0,0 +1,23 @@
+import redis
+import json
+from src.commands.actualizar_afiliacion import ActualizarAfiliacion
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+QUEUE_NAME = 'afiliaciones_update_queue'
+
+def main(app):
+    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+    print('Afiliaciones consumer iniciado, escuchando eventos...')
+    actualizar_cmd = ActualizarAfiliacion()
+    with app.app_context():
+        while True:
+            _, event = r.blpop(QUEUE_NAME)
+            data = json.loads(event)
+            afiliacion_id = data.get('afiliacion_id')
+            update_data = data.get('update_data')
+            if afiliacion_id and update_data:
+                print(f'Actualizando afiliación {afiliacion_id} con datos: {update_data}')
+                actualizar_cmd.execute(afiliacion_id, update_data)
+            else:
+                print('Evento inválido recibido:', data)
