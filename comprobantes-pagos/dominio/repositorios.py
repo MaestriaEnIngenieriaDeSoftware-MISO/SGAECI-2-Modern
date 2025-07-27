db83a39d77d3334da7bf19b86b5c98cfee3d308b
Juan De Jesus <j.dejesus@uniandes.edu.co>
2025-07-26 23:24:30 -0500

    Agrega comandos y refactoriza codigo

--- comprobantes-pagos/dominio/repositorios.py
+++
@@ -1,7 +0,0 @@
-"""
-from abc import ABC
-from saludtechalpes.seedwork.dominio.repositorios import Repositorio
-
-class RepositorioImagenes(Repositorio, ABC):
-    ...
-"""
--- comprobantes-pagos/run-comprobantes-app.sh
+++
@@ -1,3 +0,0 @@
-python3 -m pipenv shell
-pipenv install
-flask --app api/__init__.py run
