4453ebb060067bbc36d27ad67c29dc0c1d67c864
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2025-07-27 18:57:28 -0500

    refactor: move ActualizarAfiliacion to commands and add Redis consumer for updates

--- afiliaciones/src/consumers/afiliaciones_consumer.py
+++ afiliaciones/src/consumers/afiliaciones_consumer.py
@@ -1,10 +1,11 @@
 import redis
 import json
+import os
 from src.commands.actualizar_afiliacion import ActualizarAfiliacion
 
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-QUEUE_NAME = 'afiliaciones_update_queue'
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+QUEUE_NAME = os.environ.get('REDIS_QUEUE', 'afiliaciones_update_queue')
 
 def main(app):
     r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
